bf9ba2abe44c24abc01515d9a682bb2c818ddc8e
unknown <toshiba@.(none)>
2011-01-05 15:51:35 +0800

    12345

--- src/core/mod_bootstrap/core.c
+++ src/core/mod_bootstrap/core.c
@@ -1,6 +1,27 @@
-#include "listen.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "core.h"
 
-void start( char * args ){
+
+static void _init_register();
+static void _init_config();
+static void _init_so();
+static void _init_mpm();
+
+
+static rhttp_mod_bootstrap * _instance;
+
+rhttp_mod_bootstrap * rhttp_mod_bootstrap_instance(){
+
+	if( _instance == NULL ){
+		_instance = ( rhttp_mod_bootstrap *)malloc( sizeof( rhttp_mod_bootstrap ) );
+		_instance->start =_start;
+	}
+
+	return _instance;
+}
+
+static void _start( int argc, char * argv[] ){
     
     _init_register();
     _init_config();
@@ -10,19 +31,19 @@ void start( char * args ){
     
 }
 
-static void init_register(){
+static void _init_register(){
     extern rhttp_mod_register * rhttp_register   = mod_register_instance(); 
 }
 
-static void init_config(){
-    extern rhttp_mod_config *   rhttp_config     = mod_config_instance();
+static void _init_config(){
+    //extern rhttp_mod_config *   rhttp_config     = mod_config_instance();
 }
 
-static void init_so(){
-    extern rhttp_mod_so     *   rhttp_so         = mod_so_instance();
+static void _init_so(){
+    //extern rhttp_mod_so     *   rhttp_so         = mod_so_instance();
 }
 
-static void init_mpm(){
-    char * mpm_name = "multhread";
-    rhttp_mod_mpm * rhttp_mod_mpm = rhttp_mod_mpm_factory(mpm_name);
+static void _init_mpm(){
+    //char * mpm_name = "multhread";
+    //rhttp_mod_mpm * rhttp_mod_mpm = rhttp_mod_mpm_factory(mpm_name);
 }
--- src/main.c
+++ src/main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "core/mod_bootstrap/core.h"
+#include "core/mod_register/core.h"
 
 //全局变量
 rhttp_mod_bootstrap * rhttp_bootstrap;
 rhttp_mod_register  * rhttp_register;
-rhttp_mod_config    * rhttp_config;
-rhttp_mod_so        * rhttp_so;
-rhttp_mod_mpm       * rhttp_mpm;
+//rhttp_mod_config    * rhttp_config;
+//rhttp_mod_so        * rhttp_so;
+//rhttp_mod_mpm       * rhttp_mpm;
 
 
-int main( args ){
+int main( int argc, char * argv[] ){
 
     rhttp_bootstrap = rhttp_mod_bootstrap_instance();
-    rhttp_bootstrap->start( args );
+    rhttp_bootstrap->start( argc , argv );
 
 }
 
